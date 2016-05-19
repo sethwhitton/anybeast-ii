36c19f688fafdfd70675f26bc8faaafea43d8d2e
Seth Whitton <sethwhitton@swingingkazoo.local>
2016-05-19 14:15:44 -0400

    Frame position 99% complete with 1 bug:
    - Frame resizes a hint when the JS fires

--- public/javascript/frame.js
+++ public/javascript/frame.js
@@ -1,30 +1,49 @@
 
 $(document).ready(function(){
-  var $elBegin = $('#about')
-  var $elEnd = $('#about')
 
-  var scrollBegin = $elBegin.offset().top;
-  var scrollEnd = $elEnd.position().top + $elEnd.outerHeight(true);
-  $(window).on("resize scroll",function() {
+  var $elBegin = $('#about'),
+      $elEnd = $('.disciplines'),
+      $frameWrap = $('.frame-wrap'),
+      scrollBegin = $elBegin.offset().top,
+      scrollEnd = $elEnd.offset().top,
+      scrollTop,
+      docHeight,
+      frameHeight,
+      framePadding;
 
-    var scrollTop = $(window).scrollTop();
-    var winHeight = $(window).height();
-    var frameHeight = $('.frame').height();
-    var framePadding = winHeight - frameHeight / 2;
+  function calculateValues() {
+    scrollTop = $(window).scrollTop();
+    docHeight = window.innerHeight;
+    frameHeight = $('.frame').height();
+    framePadding = (docHeight - frameHeight) * .5;
+  }
 
-    // $('.frame-wrap').css('padding-top', (winHeight - frameHeight) * .5 + 'px')
-
+  function positionFrame() {
     if (scrollTop >= scrollBegin) {
-      $('.frame-wrap').addClass('active');
-    }
+      $frameWrap.addClass('active');
+      $frameWrap.css('padding-top', framePadding + 'px');
+      }
     if (scrollTop < scrollBegin) {
-      $('.frame-wrap').removeClass('active');
-    }
+      $frameWrap.removeClass('active');
+      }
     if (scrollTop >= scrollEnd) {
-      $('.frame-wrap').removeClass('active');
+      $frameWrap.removeClass('active');
+      $frameWrap.addClass('end-active');
+      }
+    if (scrollTop < scrollEnd) {
+      $frameWrap.removeClass('end-active');
+      }
     }
 
-    console.log('Window height is ' + winHeight);
+  $(window).on("resize",function() {
+    calculateValues();
+  });
+
+  $(window).on("scroll",function() {
+    calculateValues();
+    positionFrame();
+
+    console.log('Window height is ' + docHeight);
     console.log('Frame height is ' + frameHeight);
     console.log('Frame padding is ' + framePadding);
     console.log('----------------');
@@ -33,85 +52,50 @@ $(document).ready(function(){
     console.log('Scroll top is ' + scrollTop);
     console.log('----------------');
 
-
   });
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-// 	var s = $(".frame");
-// 	var pos = s.position();
-// 	var stickermax = $(document).outerHeight() - $("#about").outerHeight() - s.outerHeight() // - 40; //40 value is the total of the top and bottom margin
-//
-//   $(window).scroll(function() {
-// 		var windowpos = $(window).scrollTop();
-//     var frameCenter = $(".frame").outerHeight / 2;
-//     var viewCenter = $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2
-//
-//
-// 		if (windowpos >= pos.top && windowpos < stickermax) {
-// 			s.attr("style", ""); //kill absolute positioning
-// 			s.addClass("stick"); //stick it
-// 		} else if (windowpos >= stickermax) {
-// 			s.removeClass(); //un-stick
-// 			s.css({position: "absolute", top: stickermax + "px"}); //set sticker right above the footer
-//
-// 		} else {
-// 			s.removeClass(); //top of page
-// 		}
-// 	});
-	//alert(stickermax); //u
-
-
-
-
-// 1. Wait till the DOM is loaded
-// 2. Get the offsetTop value of the element you want to trigger the positioning change of the frame (distance from the top of the page)
-// 3. Listen for user scroll event
-// 4. When the scroll position (window.scrollTop I think) is greater than or equal to the element's position, add the class to absolutely position the frame
-// 5. When scroll position is less than element position, remove that class
-//
 // $(document).ready(function(){
+
+//   var $elBegin = $('#about')
+//   var $elEnd = $('.disciplines')
+//   var scrollBegin = $elBegin.offset().top;
+//   var scrollEnd = $elEnd.offset().top
 //
 //   $(window).on("resize scroll",function() {
+//
 //     var scrollTop = $(window).scrollTop();
+//     var docHeight = window.innerHeight;
+//     var frameHeight = $('.frame').height();
+//     var framePadding = docHeight - frameHeight / 2;
 //
-//     var horizontalCenter = Math.floor(window.innerWidth/2);
-//     var verticalCener = Math.floor(window.innerHeight/2);
-//
-//     var frameOffset = $('.frame-content').offset().top - ($('.frame').height() / 2);
-//     var endOffset = $('p.end').offset().top;
-//
-//     var frameHeight = Math.round( $('.frame').outerHeight() );
-//     var frameWidth = Math.round( $('.rigging').outerWidth() );
-//
-//
-//     if (scrollTop >= frameOffset) {
-//       $('.active').css('width', frameWidth);
-//       $('.frame').addClass('active');
+//     if (scrollTop >= scrollBegin) {
+//       $('.frame-wrap').addClass('active');
+//       $('.frame-wrap').css('padding-top', (docHeight - frameHeight) * .5 + 'px');
 //     }
-//     if (scrollTop < frameOffset || scrollTop >= endOffset) {
-//         $('.frame').removeClass('active');
-//       }
+//     if (scrollTop < scrollBegin) {
+//       $('.frame-wrap').removeClass('active');
+//     }
+//     if (scrollTop >= scrollEnd) {
+//       $('.frame-wrap').removeClass('active');
+//       $('.frame-wrap').addClass('end-active');
+//     }
+//     if (scrollTop < scrollEnd) {
+//       $('.frame-wrap').removeClass('end-active');
+//
+//     }
+//
+//     console.log('Window height is ' + docHeight);
+//     console.log('Frame height is ' + frameHeight);
+//     console.log('Frame padding is ' + framePadding);
+//     console.log('----------------');
+//     console.log('Scroll begins at ' + scrollBegin);
+//     console.log('Scroll end at ' + scrollEnd);
+//     console.log('Scroll top is ' + scrollTop);
+//     console.log('----------------');
+//
 //
 //   });
 // });
