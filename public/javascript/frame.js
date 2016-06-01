bc9a3a6c8d648cf022020ab14a400cd0301c64f8
Seth Whitton <sethwhitton@swingingkazoo.local>
2016-06-01 10:17:49 -0400

    Optimizations to Frame.js and css tweaks

--- public/javascript/frame.js
+++ public/javascript/frame.js
@@ -1,14 +1,21 @@
 
 $(document).ready(function(){
 
+      // Frame Element
   var begin = $('.frame-start'),
       frame = $('.frame-wrap'),
       border = $('.border'),
       end = $('.shapes'),
 
-      modal1 = $('#broadside > .border')
-      modal2 = $('#disciplines > .box')
-      modal3 = $('#fresh-eye > .box')
+      // Overlays
+      modal1 = $('#broadside > .border'),
+      modal2 = $('#disciplines > .box'),
+      modal3 = $('#fresh-eye > .box'),
+
+      // Transitions
+      introduction = $('.introduction'),
+      how_we_work = $('.how-we-work'),
+      our_disciplines = $('.our-disciplines');
 
 
   begin.waypoint({
@@ -37,18 +44,32 @@ $(document).ready(function(){
     },
     offset: '50%'
   });
+  //
+  // introduction.waypoint({
+  //   handler: function(direction) {
+  //     if (direction == 'down') {
+  //       frame.removeClass('sticky');
+  //       frame.addClass('end-sticky');
+  //     }
+  //     else {
+  //       frame.removeClass('end-sticky');
+  //       frame.addClass('sticky');
+  //     }
+  //   },
+  //   offset: '50%'
+  // });
 
 
-    modal1.click(function() {
-      $('.box1').toggleClass('active','modal');
-    });
+  modal1.click(function() {
+    $('.box1').toggleClass('active','modal');
+  });
 
-    $('#disciplines > .box').click(function() {
-      $('.box2').toggleClass('active');
-    });
+  modal2.click(function() {
+    $('.box2').toggleClass('active');
+  });
 
-    $('#fresh-eye > .box').click(function() {
-      $('.box3').toggleClass('active');
-    });
+  modal3.click(function() {
+    $('.box3').toggleClass('active');
+  });
 
 });
