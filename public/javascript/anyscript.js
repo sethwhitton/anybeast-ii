d994c40e14d5aa4b4edc2aaa81e3a7baff346269
sethwhitton <seth@anybeast.com>
2017-02-14 16:19:20 -0500

    About frame

--- public/javascript/anyscript.js
+++ public/javascript/anyscript.js
@@ -6,11 +6,9 @@ $(document).ready(function(){
     toggle.click(function() {
       $('.overlay').toggleClass('open');
       });
-    // Mousover Images for Menu Overlay
-    // $('#neo-mouse-over').hover(function() { 
-    //   $('.overlay').removeClass('normal-state').addClass('neo-mouse-over-effect');
-    // })
-    // .mouseleave(function(){
-    //   $('.overlay').removeClass('neo-mouse-over-effect').addClass('normal-state');
-    // });
+
+    $('.main-gallery').flickity({
+      cellAlign: 'left',
+      contain: true
+      });
 });
