d53cb74148a37f2b334032678534ea9143ff23f3
sethwhitton <seth@anybeast.com>
2017-02-17 17:41:04 -0500

    Waypoints.js hookup

--- public/javascript/anyscript.js
+++ public/javascript/anyscript.js
@@ -1,14 +1,40 @@
 $(document).ready(function(){
   
-    var toggle = $('.toggle-overlay'),
-        aside = $('.overlay');
-            
-    toggle.click(function() {
-      $('.overlay').toggleClass('open');
-      });
+// Frame Element
 
+  var about = $('.home-about'),
+      clouds = $('#clouds');
+
+// Frame and Transition Waypoints
+
+  about.waypoint({
+    handler: function() {
+      if (direction == 'down') {
+        clouds.addClass('fixed');
+      }
+      else {
+        clouds.removeClass('fixed');
+      }
+    },
+    offset: '50'
+  });
+
+
+    // Menu overlay 
+    // var toggle = $('.toggle-overlay'),
+    //     aside = $('.overlay');
+    //         
+    // toggle.click(function() {
+    //   $('.overlay').toggleClass('open');
+    //   });
+    
+          
+    // Flickity gallery
     // $('.main-gallery').flickity({
     //   cellAlign: 'left',
     //   contain: true
     //   });
+    console.log(about)
+    console.log(clouds)
+
 });
